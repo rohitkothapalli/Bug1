8af521871bd34c3dcf6eff1b72de4a7c08f02ea9
rohit kothapalli <60282687+rohitkothapalli@users.noreply.github.com>
2021-04-28 15:01:02 +0530

    Create index.js

---
+++ web/pages/index.js
@@ -0,0 +1,8 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+   <a href="https://github.com/login/oauth/authorize?client_id=f0fd634792e80e1960b0&scope=read:user,user:emailX">Login with github</a>
+  )
+}
